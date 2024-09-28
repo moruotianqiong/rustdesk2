25be7508a598244bd59ea98f8bc19756a6d56486
moruotianqiong <168299383+moruotianqiong@users.noreply.github.com>
2024-09-28 23:09:32 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rust.isfnos.xyz"];
-pub const PUBLIC_RS_PUB_KEY: &str = "9hLvUYKCuw42YNrqFzC37d9aM1u93ZJg1mn52Orz0jI=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["v62.isfnos.xyz"];
+pub const PUBLIC_RS_PUB_KEY: &str = "d8yVQnHP6F8UfMXeGJbmp0MZ0PhCBJ3Hh42RGXC1uuo=";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
